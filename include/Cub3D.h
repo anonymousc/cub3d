13ddeb36ac522ed46e89e72ec27f955457a98531
Amine Essadiki <aessadik@e2r4p9.1337.ma>
2025-01-30 09:55:24 +0100

    PUSH : fix conflict

---
+++ include/Cub3D.h
@@ -0,0 +1,16 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+typedef struct s_parsing
+{
+    char **coloring;
+    char **direction;
+    char **map;
+}               t_parsing;
+#endif
